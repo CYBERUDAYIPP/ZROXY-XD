a2767cbe33cef6a2ef3935aad547d47ff3e7db60
CYBER UDAYIPP <88606977+CYBERUDAYIPP@users.noreply.github.com>
2025-08-03 13:24:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'ZROXY-XD BOT',
+  author: '‎CYBER UDAYIPP',
+  botName: "ZROXY-XD",
+  botOwner: 'CYBER UDAYIPP', // Your name
+  ownerNumber: '918129540560', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
