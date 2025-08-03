7b6c5d91ca42f90ee8ba70d4fad05de5631599d9
CYBER UDAYIPP <88606977+CYBERUDAYIPP@users.noreply.github.com>
2025-08-03 21:36:48 +0530

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -6,8 +6,8 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '0029VaDYyTT5EjxuxuGTcg3Q@newsletter',
+            newsletterName: 'CYBER UDAYIPP',
             serverMessageId: -1
         }
     }
