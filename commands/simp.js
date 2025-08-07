5e663ee1827beb73ae4ec2e80170b2364699fc69
CYBER UDAYIPP <88606977+CYBERUDAYIPP@users.noreply.github.com>
2025-08-08 02:34:32 +0530

    Update simp.js

--- commands/simp.js
+++ commands/simp.js
@@ -37,8 +37,8 @@ async function simpCommand(sock, chatId, quotedMsg, mentionedJid, sender) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '0029VaDYyTT5EjxuxuGTcg3Q@newsletter',
+                    newsletterName: 'CYBER UDAYIPP',
                     serverMessageId: -1
                 }
             }
@@ -52,8 +52,8 @@ async function simpCommand(sock, chatId, quotedMsg, mentionedJid, sender) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '0029VaDYyTT5EjxuxuGTcg3Q@newsletter',
+                    newsletterName: 'CYBER UDAYIPP',
                     serverMessageId: -1
                 }
             }
