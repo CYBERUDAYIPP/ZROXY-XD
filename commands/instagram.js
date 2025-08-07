8523c890399a05b806646f062bd4510d0468fbab
CYBER UDAYIPP <88606977+CYBERUDAYIPP@users.noreply.github.com>
2025-08-07 16:54:21 +0530

    Update instagram.js

--- commands/instagram.js
+++ commands/instagram.js
@@ -70,14 +70,12 @@ async function instagramCommand(sock, chatId, message) {
                 await sock.sendMessage(chatId, {
                     video: { url: mediaUrl },
                     mimetype: "video/mp4",
-                    caption: "Ｄｏｗｎｌｏａｄｅｄ Ｂｙ ＺＲＯＸＹ－ＸＤ
-"
+                    caption: "𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗗 𝗕𝗬 𝗞𝗡𝗜𝗚𝗛𝗧-𝗕𝗢𝗧"
                 }, { quoted: message });
             } else {
                 await sock.sendMessage(chatId, {
                     image: { url: mediaUrl },
-                    caption: "Ｄｏｗｎｌｏａｄｅｄ Ｂｙ ＺＲＯＸＹ－ＸＤ
-                        "
+                    caption: "𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗗 𝗕𝗬 𝗞𝗡𝗜𝗚𝗛𝗧-𝗕𝗢𝗧"
                 }, { quoted: message });
             }
         }
