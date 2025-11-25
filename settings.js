8734e909231a152a5f36fb2043bb5f484dc413ab
CYBER UDAYIPP <88606977+CYBERUDAYIPP@users.noreply.github.com>
2025-11-26 00:37:35 +0530

    Modify botName and botOwner in settings.js
    Updated bot name and owner in settings.

--- settings.js
+++ settings.js
@@ -1,13 +1,16 @@
 const settings = {
   packname: 'ZROXY-XD BOT',
   author: '‎CYBER UDAYIPP',
-  botName: "ZROXY-XD",
-  botOwner: 'CYBER UDAYIPP', // Your name
+  botName: "ZROXY-XD BOT",
+  botOwner: 'CYBERUDAYIPP', // Your name
   ownerNumber: '918129540560', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
